c8b029f16b80dd0fc8cb080c4d8f6ab36450cbb6
Matthew Cotton <instinkt@gmail.com>
2025-04-12 15:17:30 +1000

    chore: Example now builds.
    Still working out conan issues, but until that is solved this allows the
    example to build properly.

--- example/conanfile.py
+++ example/conanfile.py
@@ -9,20 +9,8 @@ class CanyonExample(ConanFile):
 
     def requirements(self):
         self.requires("canyon/0.1.0")
-        # self.requires("sdl/2.28.3")
-        # self.requires("sdl_image/2.0.5")
-        # self.requires("sdl_ttf/2.20.2")
-        # self.requires("glfw/3.3.8")
-        # self.requires("libpng/1.6.42", override=True)
-        # self.requires("nlohmann_json/3.11.2")
-        # self.requires("vulkan-headers/1.3.243.0")
-        # self.requires("vulkan-loader/1.3.243.0")
-        # self.requires("vulkan-memory-allocator/3.0.1")
-        # self.requires("freetype/2.13.2")
-        # self.requires("spdlog/1.12.0")
-        # self.requires("harfbuzz/8.3.0")
-        # self.requires("moth_ui/0.1")
-        # self.requires("fmt/10.2.1", override=True)
+        self.requires("nlohmann_json/3.11.2")
+        self.requires("vulkan-loader/1.3.243.0")
 
     def build_requirements(self):
         self.tool_requires("cmake/3.27.9")
